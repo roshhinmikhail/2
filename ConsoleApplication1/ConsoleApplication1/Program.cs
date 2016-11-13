0a4646b59d5736506504050f0fec6607a87e1bc1
Mikhail <roshinmikhail2016@yandex.ru>
2016-11-13 18:09:03 +0300

    first commit

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long u, u2, u3=1, u4=1, u5=0;
+            Console.WriteLine("Введите целое число в десятичной системе");
+            u = Convert.ToInt32(Console.ReadLine());
+            u2 = u;
+            while (u > 1) { u = u / 2; u3++; }
+            while (u3 > 0) { u5 = u5 + (u2 % 2 * u4); u2 = u2 / 2; u4 = u4 * 10; u3--; }
+            Console.WriteLine(u5);
+            Console.ReadLine();
+        }
+    }
+}
